4864a2f2c64dd56e58cc8e55fc9a8ce74dccea52
LoicBrassart <loic.brassart@gmail.com>
2022-03-22 14:03:20 +0100

    Add code about Node

---
+++ lessons/S03-Node/dojo.js
@@ -0,0 +1,10 @@
+// const chalk = require('chalk');
+import chalk from 'chalk';
+// const fromWildersJs = require('./wilders');
+// import fromWildersJs from './wilders.js';
+
+// let toto = 42;
+// console.log('En fait osef de toto !');
+// console.log(fromWildersJs.wilders.length);
+
+console.log(chalk.bgCyan(chalk.yellow('Hellow !')));
---
+++ lessons/S03-Node/wilders.js
@@ -0,0 +1,9 @@
+const cbDuFormateur = 38476395037645905;
+const wilders = ['Amar', 'Remi', 'Camille', 'Valentin', 'Valentin'];
+
+const fromWildersJs = {
+  wilders: wilders,
+  name: '2203-Lille-JS',
+};
+
+module.exports = fromWildersJs;
