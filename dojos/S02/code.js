3b2c5b81f41fa7e9a49c86d50859eb538cc1f26a
LoicBrassart <loic.brassart@gmail.com>
2022-03-22 14:03:39 +0100

    Add first dojo

--- dojos/S02/code.js
+++ dojos/S02/code.js
@@ -1,1 +1,14 @@
 console.log('Fight ! o/');
+
+// let userNumber = prompt('Entre un nombre :');
+let userNumber = 0xa;
+
+if (userNumber % 15 === 0) {
+  console.log('fizzbuzz');
+} else if (!(userNumber % 5)) {
+  console.log('buzz');
+} else if (!(userNumber % 3)) {
+  console.log('fizz');
+} else {
+  console.log(userNumber);
+}
